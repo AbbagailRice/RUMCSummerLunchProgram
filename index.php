92186df46ba96c96ea14b0e48a8b307ef3ec644c
AbbagailRice <abbagail.g.rice@gmail.com>
2026-06-23 15:27:13 -0400

    typo again

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
         </div>
         
         <div class= 'register-section'>
-            <a href="pages/register.php" class="btn-register">
+            <a href="pages/registrationPage.php" class="btn-register">
                 Request an Account.
             </a>
         </div>
