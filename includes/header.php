fddee3b55427fdbb497ccf24b94b96924e4756ca
AbbagailRice <abbagail.g.rice@gmail.com>
2026-06-27 15:28:03 -0400

    header fix

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,10 @@
 <!-- This handles logo and background-->
-<div class="background-image">
-    </div>
+<div class="background-image"></div>
 
-    <div class="logo-header">
-        <div class="logo">
-           <img src="../photos/logo.png" alt="Logo">
-        </div>
-        <h1>Summer Lunch <br> Program</h1>
+<div class="logo-header">
+    <div class="logo">
+        <img src="../photos/logo.png" alt="Logo">
     </div>
+    <h1>Summer Lunch <br> Program</h1>
 </div>
+
