1eb9a2186d955259e3fed70dc11cbd38becf54f7
AbbagailRice <abbagail.g.rice@gmail.com>
2026-06-27 15:05:54 -0400

    fixed header and side bar

--- includes/header.php
+++ includes/header.php
@@ -1,73 +1,5 @@
-<!DOCTYPE HTML>
-<html>
-
-<head>
-    <title> Header Styling </title>
-    <style>
-        :root {
-            /* base colors */
-            --color-black: #000000;
-            --color-dim-grey: #5d6769;
-            --color-white: #ffffff;
-            
-            /* lights */
-            --color-pale-sky: #bfd7ea;
-            --color-dry-sage: #b6c69d;
-            
-            /* reds and pinks */
-            --color-cotton-rose: #fdd1d3;
-            --color-cotton-candy: #fda6a9;
-            --color-vibrant-coral: #ff5a5f;
-            --color-primary-scarlet: #e50027;
-        }
-
-        .background-image {
-            position: fixed;/* dont move*/
-            top: 0;
-            left: 0;
-            width: 100vw;/*frame size */
-            height: 100vh;
-            z-index: -2; /* go to back*/
-            background-image: 
-            linear-gradient(rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.3)), 
-                url('../photos/background2.jpg');
-            background-size: cover; /* scale to size*/
-            background-position: center; /* keeps center even if cropped*/
-            background-repeat: no-repeat;
-
-        }
-
-        .logo-header {
-            position: absolute;
-            top: 20px;
-            left: 20px;
-            display: flex;
-            align-items: center; /* vertical align */
-            gap: 15px; /* spacing */
-        }
-
-        .logo img {/* sizing for the image */
-            height: 90px;
-            width: auto;
-            display: block;
-        }
-
-        .logo-header h1 {
-            margin: 0;
-            font-size: 2.2rem;
-            font-weight: 700;
-            line-height: 1.1;
-            text-transform: uppercase; /* make uppercase */
-            letter-spacing: 0.5px; /* even spacing */
-            font-family: 'Arial', sans-serif;
-        }
-
-    </style>
-</head>
-<body>
-
-    <!-- This handles logo and background-->
-    <div class="background-image">
+<!-- This handles logo and background-->
+<div class="background-image">
     </div>
 
     <div class="logo-header">
@@ -77,5 +9,3 @@
         <h1>Summer Lunch <br> Program</h1>
     </div>
 </div>
-</body>
-</html>
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,70 +1,3 @@
-
-<!DOCTYPE HTML>
-<html>
-
-<head>
-    <title> Header Styling </title>
-    <style>
-        :root {
-            /* base colors */
-            --color-black: #000000;
-            --color-dim-grey: #5d6769;
-            --color-white: #ffffff;
-            
-            /* lights */
-            --color-pale-sky: #bfd7ea;
-            --color-dry-sage: #b6c69d;
-            
-            /* reds and pinks */
-            --color-cotton-rose: #fdd1d3;
-            --color-cotton-candy: #fda6a9;
-            --color-vibrant-coral: #ff5a5f;
-            --color-primary-scarlet: #e50027;
-        }
-
-        .sidebar-navigation {
-            position: fixed; /* layout rule  */
-            top: 0;
-            right: 0; /* set it to right fixed*/
-            width: 100px;
-            height: 100vh; /* top to bottom*/
-            background-color: var(--color-primary-scarlet);
-            
-            /* alignments of the links */
-            display: flex;
-            flex-direction: column; /* stack vert */
-            align-items: center; /* centered horiz */
-            gap: 30px; /* space between each */
-            box-sizing: border-box;
-            z-index: 10; /* keep to top */
-        }
-
-        /* the link containers*/
-        .nav-item a {
-            display: block;
-            text-align: center;
-            text-decoration: none;  /* no weird line under when clicked */
-            color: var(--color-cotton-rose);
-            font-weight: bold;
-            max-width: 100px; /* Safe padding buffer width text wrapping constraint */
-            word-wrap: break-word; /*break word if it is to long for sidebar*/
-
-            font-family: 'Arial', sans-serif;
-        }
-
-        /* icons */
-        .nav-icon {
-            display: inline-block;
-            font-size: 2.4rem; /*sizing*/
-            margin-bottom: 5px; /* spacing*/
-            color: var(--color-cotton-rose);
-        }
-
-    </style>
-</head>
-<body>
-
-
 <!-- This handles the fixed right-hand red navigation column-->
 <nav class="sidebar-navigation">
     <div class="nav-item">
@@ -88,5 +21,3 @@
         </a>
     </div>
 </nav>
-</body>
-</html>
