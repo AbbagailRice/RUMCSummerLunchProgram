6d1c276ad6b2532b2c133bf731bcdcd96f872346
AbbagailRice <abbagail.g.rice@gmail.com>
2026-07-01 16:36:31 -0400

    form style

--- pages/registrationPage.php
+++ pages/registrationPage.php
@@ -22,22 +22,22 @@
                 
                 <form action="../services/register.php" method="post">
                     <label for="first_name">First Name:</label>
-                    <input type="text" id="first_name" name="first_name" required> <br>
+                    <input type="text" id="first_name" name="first_name" required>
 
                     <label for="last_name">Last Name:</label>
-                    <input type="text" id="last_name" name="last_name" required> <br>
+                    <input type="text" id="last_name" name="last_name" required>
 
                     <label for="contact">Contact:</label>
                     <input type="text" id="contact" name="contact" required><br>
 
                     <label for="has_keys"> Do you have a set of keys to the building?:</label>
-                    <input type="checkbox" id="has_keys" name="has_keys" value="1"> <br> <!--// 1 for true 0 is default set on register.php if it not 1.-->
+                    <input type="checkbox" id="has_keys" name="has_keys" value="1"> <!--// 1 for true 0 is default set on register.php if it not 1.-->
 
                     <label for="username">Username:</label>
-                    <input type="text" id="username" name="username" required><br>
+                    <input type="text" id="username" name="username" required>
 
                     <label for="password">Password:</label>
-                    <input type="password" id="password" name="password" required><br>
+                    <input type="password" id="password" name="password" required>
 
                     <button type="submit">Submit Account Request</button>
                 </form>
