5a529b5fcae3152cb09b4ce8bc5de8f612d5e341
AbbagailRice <abbagail.g.rice@gmail.com>
2026-07-01 15:56:21 -0400

    i hated it

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,28 +2,28 @@
 <nav class="sidebar-navigation">
     <div class="nav-item">
         <a href="../pages/dashboard.php">
-            <span class="nav-icon">🏠</span><br>
+            <span class="nav-icon">🏠︎</span><br>
             Home
         </a>
     </div>
 
     <div class="nav-item">
         <a href="../pages/manage_recipients.php">
-            <span class="nav-icon">👤</span><br>
+            <span class="nav-icon">ጸ</span><br>
             Recipient Management
         </a>
     </div>
 
     <div class="nav-item">
         <a href="../pages/attendance.php">
-            <span class="nav-icon">📅</span><br>
+            <span class="nav-icon">🗒</span><br>
             Attendance
         </a>
     </div>
 
     <div class="nav-item">
         <a href="../pages/inventory.php">
-            <span class="nav-icon">🍞</span><br>
+            <span class="nav-icon">⚱</span><br>
             Inventory
         </a>
     </div>
